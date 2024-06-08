aa555fb0c0fcf08a0b0578fffcb7f3e45e2e70be
Maryam faizan <118754207+maryamfaizan53@users.noreply.github.com>
2024-06-09 01:55:34 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import inquirer from "inquirer";
+let user_ans = await inquirer.prompt({
+    type: "input",
+    name: "words",
+    message: "Please Enter your sentence"
+});
+let word_count = user_ans.words.trim().split(" ").length;
+console.log(`Your sentence has ${word_count} words!`);
